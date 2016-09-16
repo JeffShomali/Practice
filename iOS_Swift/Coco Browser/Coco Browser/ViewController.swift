d096dc99c8d3cc060aa74a0518244df8eb948d47
Jeff Shomali <jeffshomali@gmail.com>
2016-09-15 22:48:56 -0700

    UI Connected to Source Code (ViewController)

--- iOS_Swift/Coco Browser/Coco Browser/ViewController.swift
+++ iOS_Swift/Coco Browser/Coco Browser/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Textfild
+    @IBOutlet weak var searchBox: UITextField!
+    
+    
+    //Result View
+    @IBOutlet weak var resultView: UIWebView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +28,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Listeners Here
+    //Back Button
+    @IBAction func back(sender: AnyObject) {
+    }
+    //Go Button
+    @IBAction func go(sender: AnyObject) {
+    }
+    //Forward Button
+    @IBAction func forward(sender: AnyObject) {
+    }
+    
 }
 
