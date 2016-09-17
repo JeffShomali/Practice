e41e0711c1415818473bed972f3b11bd25f67b39
Jeff Shomali <jeffshomali@gmail.com>
2016-09-16 21:11:17 -0700

    Convert to Swift 3

--- iOS_Swift/Coco Browser/Coco Browser/ViewController.swift
+++ iOS_Swift/Coco Browser/Coco Browser/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
     
     //Textfild
     @IBOutlet weak var searchBox: UITextField!
@@ -20,21 +20,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+         self.searchBox.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+   
 
     
     //Back Button
-    @IBAction func back(sender: AnyObject) {
+    @IBAction func back(_ sender: AnyObject) {
          resultView.goBack()
     }
     
+
+    
+    func textFieldShouldReturn(_ searchBox: UITextField) -> Bool {
+        
+        searchBox.resignFirstResponder()
+        go(searchBox.text! as AnyObject)
+        return true
+    }
+    
+    
     //Go Button
-    @IBAction func go(sender: AnyObject) {
+    @IBAction func go(_ sender: AnyObject) {
         if searchBox.text == "" {  //if searchbox is emty return nothing
             return
         }
@@ -43,14 +56,14 @@ class ViewController: UIViewController {
         }
         
         //VLIDATION ***
-        if text.rangeOfString(".") != nil{  //if user is looking for website like .com , .gov, .biz means if have DOT pattern making formatted url = http://www.jeffshomali.com
-             var formattedURL = text.lowercaseString    //JEFFSHOMALI.COM = jeffshomali.com
-            if formattedURL.rangeOfString("http://") == nil {
+        if text.range(of: ".") != nil{  //if user is looking for website like .com , .gov, .biz means if have DOT pattern making formatted url = http://www.jeffshomali.com
+             var formattedURL = text.lowercased()    //JEFFSHOMALI.COM = jeffshomali.com
+            if formattedURL.range(of: "http://") == nil {
                 formattedURL = "http://\(formattedURL)"
             }
             self.loadingURL(formattedURL) //then pass constant text into loadingURL function
         } else {
-            let searchString  = text.stringByReplacingOccurrencesOfString(" ", withString: "+")
+            let searchString  = text.replacingOccurrences(of: " ", with: "+")
             let formattedURL = "https://www.google.com/search?q=\(searchString)"
             self.loadingURL(formattedURL)
         }
@@ -58,15 +71,15 @@ class ViewController: UIViewController {
         
     }
     //Funcion to Load the URL
-    func loadingURL(_url: String){
-        let url = NSURL(string: _url)!
-        let request = NSURLRequest(URL: url) //passing url 
+    func loadingURL(_ _url: String){
+        let url = URL(string: _url)!
+        let request = URLRequest(url: url) //passing url 
         //force resultView load the URL request data
         resultView.loadRequest(request);
     }
     
     //Forward Button
-    @IBAction func forward(sender: AnyObject) {
+    @IBAction func forward(_ sender: AnyObject) {
         resultView.goForward()
     }
     
