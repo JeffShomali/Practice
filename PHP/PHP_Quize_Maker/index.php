0e52abd7df3665887b85a77f7833c2985b4b876e
Jeff Shomali <jeffshomali@gmail.com>
2016-09-20 16:33:09 -0700

    #1 First Commit
    index.php: adding skeleton and redirect location
    css/style.css: adding basic style

---
+++ PHP/PHP_Quize_Maker/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>PHP Quiz Maker</title>
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1>PHP Quize Maker</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <h2>Test Your PHP Knowledge</h2>
+            <p>  This is a multiple chooice quiz to test your knowledge of PHP</p>
+            <ul>
+                 <li><strong>Number of Questions</strong> 5</li>
+                 <li><strong>Type: </strong> Multiple Choice</li>
+                 <li><strong>Estimated Time</strong> 4 Minutes</li>
+            </ul>
+            <a href="question.php?n=1" class="start">Start Quiz</a>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container">
+            Copywrite &copy; 2016, PHP Quizzer
+        </div>
+    </footer>
+
+</body>
+
+</html>
