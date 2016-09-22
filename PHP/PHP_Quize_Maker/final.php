2251f34b55eac088d93f7edd81b867a2ed68d60c
Jeff Shomali <jeffshomali@gmail.com>
2016-09-21 18:31:44 -0700

    Work with adding question page
    add.php : is like admin to adding a question into database.

---
+++ PHP/PHP_Quize_Maker/final.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>PHP Quiz Maker</title>
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1>PHP Quize Maker</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <h2>You're Done!</h2>
+            <p>Congrats you have completed the test.</p>
+            <p>Final Score: 5 </p>
+
+            <a href="question.php?n=1" class="start">Take Again</a>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container">Copywrite &copy; 2016, PHP Quizzer </div>
+    </footer>
+
+</body>
+
+</html>
