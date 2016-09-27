1ad72e908f548205c65bdf42247ac7b71f911b52
Jeff Shomali <jeffshomali@gmail.com>
2016-09-26 23:06:25 -0700

    Working With Profile Component
    index.html: Adding jQuery CDN
    APP.JSX: Adding getUserData() function that is called Github API and
    retrieve user information.Then save it userData[] array for later
    access.
    Profile.jsx: Adding bootstrap UI and access to userData[] array, then
    inserted them into placeholder.

--- React & Flux/GithubDevelopers/src/components/App.jsx
+++ React & Flux/GithubDevelopers/src/components/App.jsx
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import Profile from './github/Profile.jsx';
 
 class App extends Component{
      //instead of use getInitialState we use constructor
      constructor(props){
           super(props); // ?
           this.state = {
-               username: 'jeffshomali',
+               username: 'JeffShomali',
                userData: [],
                userRepos: [],
                perPage: 5
           }
      }
+     //Get user data form github
+     getUserData(){
+          $.ajax({
+               url: 'https://api.github.com/users/'+this.state.username+'?client_id=' +this.props.client_id + '&client_secret='+ this.props.clientSecret,
+               dataType: 'json',
+               cashe: false,
+               success: function(data){
+                    this.setState({userData: data});
+                    console.dir(data);
+               }.bind(this),
+               error: function(xhr, status, err){
+                    this.setState({username: null});
+                    alert(err);
+               }.bind(this)
+          });
+     }
+
+     componentDidMount(){
+     	this.getUserData();
+     }
+
      render(){
           return(
                <div>
-                    {this.state.username}
-                    {this.props.clientId}
+               <Profile userData = {this.state.userData} />
                </div>
           )
      }
