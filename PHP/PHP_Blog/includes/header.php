501d093fef9299caa1479e3294b0a6b201123f29
Jeff Shomali <jeffshomali@gmail.com>
2016-09-28 12:10:33 -0700

    Working With Frontend UI
    Using Bootstrap Framework for UI.

---
+++ PHP/PHP_Blog/includes/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Welcome to Jeff Blog</title>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="index.php">Home</a>
+          <a class="blog-nav-item" href="post.php">All Posts</a>
+
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="blog-header">
+           <div class="logo"><img src="images/php-logo1.png" /> </div>
+        <h1 class="blog-title">Jeff Personal Blog</h1>
+        <p class="lead blog-description">All about PHP.</p>
+      </div>
+
+      <div class="row">
+
+        <div class="col-sm-8 blog-main">
