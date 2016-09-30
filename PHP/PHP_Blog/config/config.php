c122b0f0952d6c62432cf9e2ee2df23dd2849865
Jeff Shomali <jeffshomali@gmail.com>
2016-09-29 18:56:11 -0700

    Create About in config file

--- PHP/PHP_Blog/config/config.php
+++ PHP/PHP_Blog/config/config.php
@@ -7,3 +7,5 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_NAME', 'phpblog');
+
+$site_description = 'Full Stack Web Developer, Technologist, Blogger, Web Entrepreneur, Loves Gadgets and FOREX trader at FXCM. Addicted to Starbucks';
