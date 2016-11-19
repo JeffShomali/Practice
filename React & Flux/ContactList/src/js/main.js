77e410ec8403469f36f8b0dd21bfbad7e1c696c7
Jeff Shomali <jeffshomali@gmail.com>
2016-11-18 19:40:45 -0800

    Adding Folder Structure

---
+++ React & Flux/ContactList/src/js/main.js
@@ -0,0 +1,9 @@
+var APP      = require('./components/App');
+var Raect    = require('react');
+var ReactDom = require('react-dom');
+var AppAPI   = require('./utils/appAPI.js');
+
+ReactDOM.rennder(
+     <APP />,
+     getElementById('app')
+);
