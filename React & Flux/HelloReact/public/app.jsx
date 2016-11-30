5bb1203a9f1d4fbba77a201742f3fb1d2b028d01
Jeff Shomali <jeffshomali@gmail.com>
2016-11-29 23:15:13 -0800

    Working With Form in React

---
+++ React & Flux/HelloReact/public/app.jsx
@@ -0,0 +1,55 @@
+
+var Greater = React.createClass({
+     getDefaultProps: function (){
+          return {
+               name: "Name props not passed",
+               message: "Message props not passed"
+          };
+     },
+
+     getInitialState: function () {
+          return {
+               name: this.props.name
+          };
+     },
+
+     onButtonClick: function (e){
+          e.preventDefault();
+
+          var nameRef = this.refs.name;
+          var name = nameRef.value;
+          nameRef.value = '';
+
+          if(typeof name == 'string' && name.length > 0) {
+               this.setState({
+                    name: name
+               });
+          }
+     },
+
+
+
+     render: function(){
+          var name = this.state.name;
+          var message =  this.props.message;
+          return (
+               <div>
+                    <h1>Hello {name}!</h1>
+                    <p> {message + " !!"}</p>
+
+               <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name"/>
+                    <button>Set Name</button>
+               </form>
+               </div>
+          );
+     }
+});
+
+var name = "Jeff";
+var message  = "This is messsage from a components"
+
+ReactDOM.render(
+     <Greater name={name} message={message}/>,
+     document.getElementById('app')
+);
